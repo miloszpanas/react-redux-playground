dad4851e5ebea0a8d99c5729551c61cdc2b9d626
Panas Miłosz <milosz@linacti.pl>
2019-03-03 14:47:41 +0100

    pass dispatch function to componentDidMount, giving it access to state via props

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
-import { connect } from redux;
+import { connect } from "react-redux";
 import PropTypes from 'prop-types';
 import { GET_CONTACTS } from '../../actions/types';
 
 
 class Contacts extends Component {
 
+  componentDidMount() {
+    this.props.getContacts();
+  }
+
   render() {
-    const { contacts } = this.state;
+    const { contacts } = this.props;
     return (
       <React.Fragment>
         <h1 className="display-4 mb-2">
